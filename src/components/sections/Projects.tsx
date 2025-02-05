14c1d1fb692b12b03bbf233b1b4424f5c0efc6fd
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2025-02-05 21:22:37 +0200

    made some changes on clients section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Project } from '@/interfaces/project';
 import { AppDispatch } from '@/redux/store';
 import { getAllProjects } from '@/services/client';
 import Link from 'next/link';
@@ -39,68 +40,26 @@ const Projects: React.FC = () => {
                     <SkeletonProjectCard key={index} />
                 ))
                 ) : (
-                <div className="md:w-[90%] w-full mx-auto grid grid-cols-1 md:grid-cols-7 gap-8 mt-24">
-                    <div 
-                        className="flex items-end md:col-span-4 bg-cover bg-center bg-black bg-opacity-50 md:h-80 h-64 rounded-xl cursor-pointer"
-                        style={{ backgroundImage: `url(${projects[0]?.thumbnailAfter})` }}
-                        onClick={() => handleProjectClick(projects[0]?._id)}
-                    >
-                        <div className="md:w-3/4 w-full h-20 m-5 py-3 px-5 flex flex-col justify-center bg-black bg-opacity-50 backdrop-blur-sm rounded-xl cursor-pointer">
-                            <div className="flex justify-between text-white">
-                                <span className='uppercase font-medium text-sm text-white'>{projects[0]?.name}</span>
-                                <span className='redex text-sm font-[300]'>{projects[0]?.areaSize} {projects[0]?.areaUnit}</span>
+                    <div className="md:w-[90%] w-full mx-auto grid grid-cols-1 md:grid-cols-7 gap-8 mt-24">
+                        {projects.map((project:Project, index:number) => (
+                            <div 
+                                key={project._id}
+                                className={`flex items-end ${index % 4 === 0 || index % 4 === 3 ? 'md:col-span-4' : 'md:col-span-3'} bg-cover bg-center bg-black bg-opacity-50 md:h-80 h-64 rounded-xl cursor-pointer`}
+                                style={{ backgroundImage: `url(${project.thumbnailAfter})` }}
+                                onClick={() => handleProjectClick(project._id)}
+                            >
+                                <div className="md:w-3/4 w-full h-20 m-5 py-3 px-5 flex flex-col justify-center bg-black bg-opacity-50 backdrop-blur-sm rounded-xl cursor-pointer">
+                                    <div className="flex justify-between text-white">
+                                        <span className='uppercase font-medium text-sm text-white'>{project.name}</span>
+                                        <span className='redex text-sm font-[300]'>{project.areaSize} {project.areaUnit}</span>
+                                    </div>
+                                    <div className="text-white mt-2">
+                                        <p className='text-white text-xs font-[300]'> <span className='text-[#9E9E9E] mr-2'>Client</span>{project.clientId.name}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="text-white mt-2">
-                                <p className='text-white text-xs font-[300]'> <span className='text-[#9E9E9E] mr-2'>Client</span>{projects[0]?.clientId.name}</p>
-                            </div>
-                        </div>
+                        ))}
                     </div>
-                    <div 
-                        className="flex items-end justify-end md:col-span-3 bg-cover bg-center bg-black bg-opacity-50 md:h-80 h-64 rounded-xl cursor-pointer" 
-                        style={{ backgroundImage: `url(${projects[1]?.thumbnailAfter})` }}
-                        onClick={() => handleProjectClick(projects[1]?._id)}    
-                    >
-                        <div className="md:w-[80%] w-full h-20 m-5 py-3 px-5 flex flex-col justify-center bg-black bg-opacity-50 backdrop-blur-sm rounded-xl cursor-pointer">
-                            <div className="md:w-full flex justify-between text-white">
-                                <span className='md:w-[60%] uppercase font-medium text-sm text-white'>{projects[1]?.name}</span>
-                                <span className='md:w-[22%] redex text-sm font-[300]'> {projects[1]?.areaSize} {projects[1]?.areaUnit}</span>
-                            </div>
-                            <div className="text-white mt-2">
-                                <p className='text-white text-xs font-[300]'> <span className='text-[#9E9E9E] mr-2'>Client</span>{projects[2]?.clientId.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div 
-                        className="flex items-end md:col-span-3 bg-cover bg-center bg-black bg-opacity-50 md:h-80 h-64 rounded-xl" 
-                        style={{ backgroundImage: `url(${projects[2]?.thumbnailAfter})` }}
-                        onClick={() => handleProjectClick(projects[2]?._id)}    
-                    >
-                        <div className="md:w-3/4 w-full h-20 m-5 py-3 px-5 flex flex-col justify-center bg-black bg-opacity-50 backdrop-blur-sm rounded-xl">
-                            <div className="flex justify-between text-white">
-                                <span className='uppercase font-medium text-sm text-white'>{projects[2]?.name}</span>
-                                <span className='redex text-sm font-[300]'> {projects[2]?.areaSize} {projects[2]?.areaUnit}</span>
-                            </div>
-                            <div className="text-white mt-2">
-                                <p className='text-white text-xs font-[300]'> <span className='text-[#9E9E9E] mr-2'>Client</span>{projects[2]?.clientId.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div 
-                        className="flex items-end justify-end md:col-span-4 bg-cover bg-center bg-black bg-opacity-50 md:h-80 h-64 rounded-xl" 
-                        style={{ backgroundImage: `url(${projects[3]?.thumbnailAfter})` }}
-                        onClick={() => handleProjectClick(projects[3]?._id)}    
-                    >
-                        <div className="md:w-3/4 w-full h-20 m-5 py-3 px-5 flex flex-col justify-center bg-black bg-opacity-50 backdrop-blur-sm rounded-xl">
-                            <div className="flex justify-between text-white">
-                                <span className='uppercase font-medium text-sm text-white'>{projects[3]?.name}</span>
-                                <span className='redex text-sm font-[300]'> {projects[3]?.areaSize} {projects[3]?.areaUnit}</span>
-                            </div>
-                            <div className="text-white mt-2">
-                                <p className='text-white text-xs font-[300]'> <span className='text-[#9E9E9E] mr-2'>Client</span>{projects[3]?.clientId.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 )}
                 <div className='w-full flex justify-center'>
                     <Link 
