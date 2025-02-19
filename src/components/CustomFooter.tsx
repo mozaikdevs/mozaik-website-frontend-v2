f0e95faea92cd7477fa27985388834ae54e977c5
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2025-02-19 22:09:58 +0200

    added responsiveness to the custom layout

--- src/components/CustomFooter.tsx
+++ src/components/CustomFooter.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 const CustomFooter: React.FC = () => {
     return (
-        <footer className="w-full bg-black text-white p-20">
-            <div className='w-full flex justify-between'>
-                <div className='w-[40%]'>
-                    <h1 className='md:w-2/3 text-5xl redex font-bold'>WHAT <span className='font-normal'>WE DO</span> FOR YOU</h1>
-                    <div className='md:w-2/3 flex justify-between redex my-8'>
-                        <div className='md:w-[40%] flex flex-col text-sm'>
+        <footer className="w-full bg-black text-white md:p-20 p-5">
+            <div className='w-full flex justify-between md:flex-row flex-col'>
+                <div className='md:w-[40%] w-full'>
+                    <h1 className='w-2/3 md:text-5xl text-3xl redex font-bold'>WHAT <span className='font-normal'>WE DO</span> FOR YOU</h1>
+                    <div className='md:w-2/3 w-full flex justify-between redex my-8'>
+                        <div className='w-[40%] flex flex-col text-sm'>
                             <Link href='/services' className='my-1'>Consultancy</Link>
                             <Link href='/services' className='my-1'>Design</Link>
                             <Link href='/services' className='my-1'>Finishing and Partitioninng works</Link>
@@ -20,23 +20,23 @@ const CustomFooter: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-[40%]'>
+                <div className='md:w-[40%] w-full'>
                     <h1 className='text-2xl redex'>Lets work together</h1>
                     <form className='mt-5'>
-                        <div className='flex justify-between'>
+                        <div className='flex justify-between md:flex-row flex-col'>
                             <input 
                                 type="text" 
                                 name="" 
                                 id=""
                                 placeholder='Enter your full  name' 
-                                className='w-[45%] bg-[#1D1D1D] text-sm text-[#AEABAB] p-3'
+                                className='md:w-[45%] w-full bg-[#1D1D1D] text-sm text-[#AEABAB] p-3 md:mb-0 mb-3'
                             />
                             <input 
                                 type="text" 
                                 name="" 
                                 id=""
                                 placeholder='Enter your Email' 
-                                className='w-[45%] bg-[#1D1D1D] text-sm text-[#AEABAB] p-3'
+                                className='md:w-[45%] w-full bg-[#1D1D1D] text-sm text-[#AEABAB] p-3'
                             />
                         </div>
                         <textarea 
@@ -62,7 +62,7 @@ const CustomFooter: React.FC = () => {
                     <Link href="#" target='_blank' className='mx-6'>LINKEDIN</Link>
                     <Link href="#" target='_blank'>twitter</Link>
                 </div>
-                <p className="md:w-[20%] mx-auto text-sm text-[#8F8F8F] redex font-[300] mt-5">&copy; {new Date().getFullYear()} All rights reserved by Mozaik concepts</p>
+                <p className="md:w-[20%] w-1/2 mx-auto text-sm text-[#8F8F8F] redex font-[300] mt-5">&copy; {new Date().getFullYear()} All rights reserved by Mozaik concepts</p>
             </div>
         </footer>
     );
